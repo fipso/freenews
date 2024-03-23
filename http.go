137096450bed2ca23401677f10deee3eba86cdf4
Matt Joiner <anacrolix@gmail.com>
2024-03-23 17:21:08 +1100

    Change domain regexp to allow domains with more than 2 components

--- http.go
+++ http.go
@@ -15,6 +15,8 @@ import (
 	"time"
 )
 
+var domainRegexp = regexp.MustCompile(`^([a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.)+[a-zA-Z]{2,}$`)
+
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Host == config.InfoHost {
 
@@ -29,11 +31,8 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 			name := r.Form.Get("name")
 
 			// Validate user input
-			re := regexp.MustCompile(
-				`^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\.[a-zA-Z]{2,}$`,
-			)
-			if !re.MatchString(name) {
-				w.Write([]byte("Error: Invalid domain"))
+			if !domainRegexp.MatchString(name) {
+				http.Error(w, "Error: Invalid domain", http.StatusBadRequest)
 				return
 			}
 
